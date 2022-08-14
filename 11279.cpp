a6f8632b687fedfe09f13e3db6f770612c92590b
7thKsJ <95681142+7thKsJ@users.noreply.github.com>
2022-08-15 02:53:07 +0900

    solved_2008kb_20ms_44397137_384b

---
+++ 11279.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+priority_queue <int, vector <int>, less <int>> q;
+
+int main() {
+	int n, num;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &num);
+		if (num != 0) {
+			q.push(num);
+		}
+		else {
+			if (q.empty()) {
+				printf("0\n");
+			}
+			else {
+                printf("%d\n", q.top());
+				q.pop();
+			}
+		}
+	}
+	return 0;
+}
